1a8ba82fb5f3a1cf56e7e85a1a154f59433c7f53
Danish <dnazir343@gmail.com>
2023-09-19 22:05:20 +0500

    fix bugs

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 // import "@/app/styles/LinkComp.css";
 import '@/app/styles/tailwind.css'
-import Steps from './components/Steps';
 import LogoCloud from './components/LogoCloud';
 import About from './components/About';
 import Instagram from "./components/Instagram";
@@ -11,7 +10,7 @@ export default function Home() {
     <>
       <Instagram />
       <Card title="Download Instagram Videos" color="bg-gradient-to-l from-pink-600 to-blue-600 bg-clip-text text-transparent"
-      src="/instagram3.png" />
+        src="/instagram3.png" />
       <About />
       <LogoCloud />
     </>
